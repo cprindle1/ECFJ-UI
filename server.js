1bac0e47a9abe29eff4d1807967fa6cc29b4fe79
555032 <charlesprindle@c02x910hjgh7.users.bah.com>
2018-12-05 12:41:15 -0500

    server.ts

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/ecfj'));
 
 app.get('/*', function (req, res) {
-
     res.sendFile(path.join(__dirname + '/dist/ecfj/index.html'));
 });
 
