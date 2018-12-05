cd5e91377e39f3b72cf82a427e28e9b1a8f1523a
555032 <charlesprindle@c02x910hjgh7.users.bah.com>
2018-12-05 14:27:24 -0500

    try again

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var app = express();
+
+// Initialize the app.
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
